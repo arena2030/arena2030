bf6eccf08a0f406392cec30b7e96418ff9f4775a
arena2030 <lorenzo.perinelli1@gmail.com>
2025-09-15 13:26:46 +0200

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -1,7 +1,5 @@
-<?php /* Footer blu scuro compatto */ ?>
 <footer class="site-footer">
   <div class="container footer-inner">
-
     <ul class="footer-menu">
       <li><a href="/chi-siamo.php">Chi siamo</a></li>
       <li><a href="/contatti.php">Contatti</a></li>
@@ -14,12 +12,9 @@
       <li><a href="/assistenza.php">Assistenza</a></li>
       <li><a href="/segnalazioni.php">Segnalazione abusi</a></li>
     </ul>
-
     <div class="footer-bottom">
       <span>© ARENA. Tutti i diritti riservati.</span>
     </div>
   </div>
   <script src="/assets/js/ui.js"></script>
 </footer>
-</body>
-</html>
