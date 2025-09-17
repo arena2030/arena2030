1500634af930a2e3fe422a1b621dc15781f81309
arena2030 <lorenzo.perinelli1@gmail.com>
2025-09-17 09:46:20 +0200

    Update header_punto.php

--- partials/header_punto.php
+++ partials/header_punto.php
@@ -33,7 +33,7 @@ $coins    = $_SESSION['coins']    ?? 0; // placeholder
       <ul class="subhdr__menu">
         <li><a class="subhdr__link" href="/punto/players.php">Players</a></li>
         <li><a class="subhdr__link" href="/punto/commissioni.php">Commissioni</a></li>
-        <li><a class="subhdr__link" href="/punto/fatture.php">Fatture</a></li>
+        <li><a class="subhdr__link" href="/punto/premi.php">Premi</a></li>
       </ul>
     </div>
   </nav>
